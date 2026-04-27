31598d3aacaadfddecda7f42923583fd59f81c58
M4TTH3 <matthewau-yeung@hotmail.com>
2026-04-27 11:14:28 -0400

    setup home manager

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "m4tth3 home-manager flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { nixpkgs, home-manager, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+    in {
+      homeConfigurations."m4tth3" = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        modules = [ ./home.nix ];
+      };
+    };
+}
---
+++ home.nix
@@ -0,0 +1,64 @@
+{ pkgs, config, ... }: {
+  home.username = "m4tth3";
+  home.homeDirectory = "/home/m4tth3";
+  home.stateVersion = "25.11";
+
+  home.packages = with pkgs; [
+    # Editor
+    neovim
+
+    # Core CLI
+    ripgrep
+    fd
+    tree-sitter
+
+    # Languages / runtimes
+    go
+    python3
+    jdk
+    nodejs
+    clang
+    clang-tools
+    rustc
+    cargo
+    rustfmt
+    clippy
+    rust-analyzer
+
+    # Linters
+    ruff
+    golangci-lint
+    eslint_d
+
+    # Formatters
+    stylua
+    prettier
+    google-java-format
+    ktfmt
+  ];
+
+  programs.home-manager.enable = true;
+
+  programs.git = {
+    enable = true;
+    signing = {
+      format = "ssh";
+      key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHVBIcjToRazH/oMS5QfTlmLyIBLB/FXZnVQRdWxSZ/w";
+      signByDefault = true;
+    };
+    settings = {
+      user.name = "M4TTH3";
+      user.email = "matthewau-yeung@hotmail.com";
+      core.editor = "vim";
+      gpg.ssh.program = "/opt/1Password/op-ssh-sign";
+      gpg.ssh.allowedSignersFile = "~/.ssh/allowed_signers";
+    };
+  };
+
+  home.sessionVariables = {
+    SSH_AUTH_SOCK = "${config.home.homeDirectory}/.1password/agent.sock";
+  };
+
+  home.file.".ssh/allowed_signers".text =
+    "matthewau-yeung@hotmail.com ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHVBIcjToRazH/oMS5QfTlmLyIBLB/FXZnVQRdWxSZ/w\n";
+}
